ea7091bc64de1961a4fc09ae81206d5e40fa2b78
Sasha Zin <zinalbo@tut.by>
2023-06-23 21:27:38 +0300

    Bundle

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -2,13 +2,20 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import io.qameta.allure.selenide.LogType;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import java.util.ResourceBundle;
 import java.util.logging.Level;
 import static com.codeborne.selenide.Browsers.CHROME;
+import static com.codeborne.selenide.Selenide.clearBrowserLocalStorage;
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class BaseTest {
+    private final ResourceBundle bundle = ResourceBundle.getBundle("test_framework");
+    private final String URL = bundle.getString("path_to_url");
+
     @BeforeClass
     public void start() {
         SelenideLogger.addListener("allure", new AllureSelenide()
@@ -16,11 +23,17 @@ public class BaseTest {
                 .savePageSource(true)
                 .enableLogs(LogType.BROWSER, Level.ALL));
         Configuration.browserSize = "1600x900";
-        Configuration.browser =CHROME;
+        Configuration.browser = CHROME;
     }
 
     @BeforeMethod
     public void goToUrl() {
-        open("https://www.saucedemo.com/");
+        open(URL);
+    }
+
+    @AfterMethod
+    public void clearData() {
+        clearBrowserLocalStorage();
+        clearBrowserCookies();
     }
 }
